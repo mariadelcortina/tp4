5d6ba8490f8b677e1762d31c1912935a67b06e7b
mariadelcortina <146002795+mariadelcortina@users.noreply.github.com>
2023-10-04 08:51:40 -0300

    Add files via upload

---
+++ codigo/nose.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){ irArriba(); }); //Hacia arriba
+
+document.getElementById("BOTONARRIBA").addEventListener("click", function() {
+    this.classList.add("visible");
+  });
